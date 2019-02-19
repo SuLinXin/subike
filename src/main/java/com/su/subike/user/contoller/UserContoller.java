fed3293b6cb142783f73d33f3cc118f67fe25ac8
SuLinXin <434945072@qq.com>
2019-02-19 23:20:31 +0800

    login第二次修改

--- src/main/java/com/su/subike/user/contoller/UserContoller.java
+++ src/main/java/com/su/subike/user/contoller/UserContoller.java
@@ -1,17 +1,19 @@
 package com.su.subike.user.contoller;
 
+import com.su.subike.common.constants.Constants;
 import com.su.subike.common.resp.ApiResult;
 import com.su.subike.user.dao.UserMapper;
 import com.su.subike.user.entity.LoginInfo;
 import com.su.subike.user.entity.User;
 import com.su.subike.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("user")
 public class UserContoller {
@@ -34,9 +36,16 @@ public class UserContoller {
 
         ApiResult<String> resp = new ApiResult<>();
 
-        String token = userService.login();
+        try{
+            String token = userService.login();
+            resp.setData(token);
+        }catch (Exception e){
+            log.error("Fail to login",e);
+            resp.setCode(Constants.RESP_STATUS_BADREQUEST);
+            resp.setMessage("login内部错误");
+        }
 
-        resp.setData(token);
+
         return resp;
     }
 
--- src/main/java/com/su/subike/user/entity/LoginInfo.java
+++ src/main/java/com/su/subike/user/entity/LoginInfo.java
@@ -1,4 +1,15 @@
 package com.su.subike.user.entity;
 
+import lombok.Data;
+
+@Data
 public class LoginInfo {
+
+    /**登录信息密文***/
+    private String data;
+
+
+    /**RSA加密的AESkey***/
+    private String key;
+
 }
