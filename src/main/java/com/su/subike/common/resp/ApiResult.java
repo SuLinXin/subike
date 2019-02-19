0095b9b594d8888bafed75c3b360c27b8b57a97f
SuLinXin <434945072@qq.com>
2019-02-19 20:41:26 +0800

    login

---
+++ src/main/java/com/su/subike/common/resp/ApiResult.java
@@ -0,0 +1,8 @@
+package com.su.subike.common.resp;
+
+public class ApiResult {
+
+    private int code = 200;
+
+    private String message;
+}
--- src/main/java/com/su/subike/user/contoller/UserContoller.java
+++ src/main/java/com/su/subike/user/contoller/UserContoller.java
@@ -2,7 +2,9 @@ package com.su.subike.user.contoller;
 
 import com.su.subike.user.dao.UserMapper;
 import com.su.subike.user.entity.User;
+import com.su.subike.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserContoller {
 
 
+//    @Autowired
+//    private UserMapper userMapper;
+//    @RequestMapping("/hello")
+//    public User hello(){
+//        User user = userMapper.selectByPrimaryKey(1L);
+//        return user;
+//    }
+
     @Autowired
-    private UserMapper userMapper;
-    @RequestMapping("/hello")
-    public User hello(){
-        User user = userMapper.selectByPrimaryKey(1L);
-        return user;
+    @Qualifier("userServiceImpl")
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public User login(){
+        String token = userService.login();
+        return token;
     }
+
 }
