b1cd7cb967cbe80ce895a84b226a102499ae5ae8
SuLinXin <434945072@qq.com>
2019-02-21 23:42:29 +0800

    登陆密钥验证

--- src/main/java/com/su/subike/user/service/UserServiceImpl.java
+++ src/main/java/com/su/subike/user/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.su.subike.user.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.su.subike.common.exception.SuBikeException;
 import com.su.subike.security.AESUtil;
 import com.su.subike.security.Base64Util;
@@ -7,8 +9,10 @@ import com.su.subike.security.RSAUtil;
 import com.su.subike.user.dao.UserMapper;
 import com.su.subike.user.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.generator.internal.util.StringUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 @Slf4j
@@ -28,6 +32,17 @@ public class UserServiceImpl implements UserService {
             if (decryptData == null){
                 throw new Exception();
             }
+
+            JSONObject jsonObject = JSON.parseObject(decryptData);
+            String mobile = jsonObject.getString("mobile");
+            String code = jsonObject.getString("code");
+            if(StringUtils.isEmpty(mobile)|| StringUtils.isEmpty(code)){
+                throw new Exception();
+            }
+            //去redis去取验证码 匹配
+
+
+            //判断用户是否在数据库，没有的话帮他注册，插入数据库，生成token存在redis
         }catch (Exception e) {
             log.error("Fail to decrypt data",e);
             e.printStackTrace();
