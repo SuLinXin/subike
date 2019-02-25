c34a5a1c59fdc254bca6f6cd73be15676ba98260
SuLinXin <434945072@qq.com>
2019-02-25 23:13:08 +0800

    用户修改昵称ok

--- src/main/java/com/su/subike/user/contoller/UserContoller.java
+++ src/main/java/com/su/subike/user/contoller/UserContoller.java
@@ -10,13 +10,16 @@ import com.su.subike.user.entity.LoginInfo;
 import com.su.subike.user.entity.User;
 import com.su.subike.user.entity.UserElement;
 import com.su.subike.user.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
 
 @Slf4j
@@ -90,6 +93,32 @@ public class UserContoller extends BaseController {
             resp.setMessage("修改昵称失败");
         }
 
+        return resp;
+    }
+
+
+    /**
+     *@Author JackWang [www.coder520.com]
+     *@Date 2017/8/5 0:59
+     *@Description 修改头像
+     */
+    @ApiOperation(value="上传头像",notes = "用户上传头像 file" ,httpMethod = "POST")
+    @RequestMapping(value = "/uploadHeadImg", method = RequestMethod.POST)
+    public ApiResult<String> uploadHeadImg(HttpServletRequest req, @RequestParam(required=false ) MultipartFile file) {
+
+        ApiResult<String> resp = new ApiResult<>();
+        try {
+            UserElement ue = getCurrentUser();
+            userService.uploadHeadImg(file,ue.getUserId());
+            resp.setMessage("上传成功");
+        } catch (SuBikeException e) {
+            resp.setCode(e.getStatusCode());
+            resp.setMessage(e.getMessage());
+        } catch (SuBikeException e) {
+            log.error("Fail to update user info", e);
+            resp.setCode(Constants.RESP_STATUS_INTERNAL_ERROR);
+            resp.setMessage("内部错误");
+        }
         return resp;
     }
 
