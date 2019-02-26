5f8e3d45692b5b16786c5aeab2ca2ddf987b41fa
SuLinXin <434945072@qq.com>
2019-02-26 23:18:23 +0800

    添加SpringSecurity实现安全校验2

---
+++ src/main/java/com/su/subike/security/RestPreAuthenticatedProcessingFilter.java
@@ -0,0 +1,25 @@
+package com.su.subike.security;
+
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @ClassName RestPreAuthenticatedProcessingFilter
+ * @Description TODO
+ * @Author 434945072@qq.com
+ * Data 2019/2/26 23:12
+ * Version 1.0
+ **/
+public class RestPreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+}
