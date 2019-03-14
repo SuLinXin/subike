dec6f462c829701d0f8befebb0c73e97c8d2a54b
SuLinXin <434945072@qq.com>
2019-03-14 22:12:27 +0800

    加入mongodb用于车辆位置状态查询2

---
+++ src/main/java/com/su/subike/bike/dao/BikeMapper.java
@@ -0,0 +1,17 @@
+package com.su.subike.bike.dao;
+
+import com.su.subike.bike.entity.Bike;
+
+public interface BikeMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Bike record);
+
+    int insertSelective(Bike record);
+
+    Bike selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Bike record);
+
+    int updateByPrimaryKey(Bike record);
+}
---
+++ src/main/java/com/su/subike/bike/entity/Bike.java
@@ -0,0 +1,43 @@
+package com.su.subike.bike.entity;
+
+public class Bike {
+    private Long id;
+
+    private Long number;
+
+    private Byte type;
+
+    private Byte enableFlag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
+
+    public Byte getEnableFlag() {
+        return enableFlag;
+    }
+
+    public void setEnableFlag(Byte enableFlag) {
+        this.enableFlag = enableFlag;
+    }
+}
